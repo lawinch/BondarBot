8f57066697bbf46eca332dd127be094777fe5f56
lawinch <lawinch@gmail.com>
2018-03-23 23:20:45 +0300

    test app

--- bondar_bot.py
+++ bondar_bot.py
@@ -24,46 +24,39 @@ class BotHandler:
 
     def get_last_update(self):
         get_result = self.get_updates()
-
         if len(get_result) > 0:
             last_update = get_result[-1]
         else:
-            last_update = get_result[len(get_result)]
+            last_update = None
 
         return last_update
 
 
 greet_bot = BotHandler('584747595:AAFcTIaf-Es5BkHIazuu3IVkBbYPfM9Mtkg')
-greetings = ('здравствуй', 'привет', 'ку', 'здорово')
+drink_offers_1 = {'айда', 'пошли', 'погнали', 'как насчет', 'давай', 'го', 'пойдет', 'пойдем'}
+drink_offers_2 = {'бухать', 'пить', 'синячить', 'выпить', 'накатить'}
 now = datetime.datetime.now()
 
 
 def main():
     new_offset = None
-    today = now.day
-    hour = now.hour
 
     while True:
         greet_bot.get_updates(new_offset)
 
         last_update = greet_bot.get_last_update()
+        if last_update is None:
+            continue
 
         last_update_id = last_update['update_id']
         last_chat_text = last_update['message']['text']
         last_chat_id = last_update['message']['chat']['id']
         last_chat_name = last_update['message']['chat']['first_name']
 
-        if last_chat_text.lower() in greetings and today == now.day and 6 <= hour < 12:
-            greet_bot.send_message(last_chat_id, 'Доброе утро, {}'.format(last_chat_name))
-            today += 1
+        words = set(last_chat_text.lower().split())
 
-        elif last_chat_text.lower() in greetings and today == now.day and 12 <= hour < 17:
-            greet_bot.send_message(last_chat_id, 'Добрый день, {}'.format(last_chat_name))
-            today += 1
-
-        elif last_chat_text.lower() in greetings and today == now.day and 17 <= hour < 23:
-            greet_bot.send_message(last_chat_id, 'Добрый вечер, {}'.format(last_chat_name))
-            today += 1
+        if len(words.intersection(drink_offers_1)) > 0 and len(words.intersection(drink_offers_2)) > 0:
+            greet_bot.send_message(last_chat_id, '{}, слушай, я сегодня пас..'.format(last_chat_name))
 
         new_offset = last_update_id + 1
 
